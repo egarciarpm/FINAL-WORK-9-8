5c5de7e04d5a3831d4638168e436497c5e1df3dd
Elijah Garcia <egarciaRPM@gmail.com>
2016-09-11 19:45:27 -0700

    jquery scripts v0.01

--- js/index.js
+++ js/index.js
@@ -4,14 +4,38 @@ $(document).ready(function(){
 
 
 
-$('.promo').hide();
+$('.promodesc').hide();
 $('.jumbotron').hide();
+$('.blogdesc').hide();
+$('.testdesc').hide();
+$('.rpmdesc').hide();
+$('#suite1desc').hide();
+$('#suite2desc').hide();
+$('#suite3desc').hide();
 $('.jumbotron').delay(250).slideDown(1500);
-$('.headerlogo').mouseover(function(){
-    $('.headertext').slideToggle(500).delay(1500);
+$('.headerlogo').click(function(){
+    $('.headerdesc').slideToggle(500).delay(2000);
 });
-$('.promo').mouseover(function(){
-    $('.promodesc').slideToggle(1500).delay(1500);
+$('.promo').click(function(){
+    $('.promodesc').slideToggle(1500).delay(2000);
+});
+$('.testhead').click(function(){
+    $('.testdesc').slideToggle(1500).delay(2000);
+});
+$('.bloghead').click(function(){
+    $('.blogdesc').slideToggle(1500).delay(2000);
+});
+$('.rpmhead').click(function(){
+    $('.rpmdesc').slideToggle(1500).delay(2000);
+});
+$('.suite1head').click(function(){
+    $('#suite1desc').slideToggle(1500).delay(2000);
+});
+$('.suite2head').click(function(){
+    $('#suite2desc').slideToggle(1500).delay(2000);
+});
+$('.suite3head').click(function(){
+    $('#suite3desc').slideToggle(1500).delay(2000);
 });
 });
 
